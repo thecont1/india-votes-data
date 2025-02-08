c70ca905a319a7227de93b4e2223681a4c0bbd34
Mahesh Shantaram <ms@thecontrarian.in>
2025-02-08 17:02:01 +0530

    moved all datasets to /results

---
+++ update_delhi_results.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run the scraper
+python3 eci-scraper2-DL.py
+
+# Get current time in HH:MM format
+current_time=$(date +"%H:%M")
+
+# Add files to git
+git add .results/2025AC-DL.csv 2025AC-DL.json
+
+# Commit with timestamp
+git commit -m "NCT of Delhi Results: $current_time"
+
+# Push to GitHub
+git push
+
